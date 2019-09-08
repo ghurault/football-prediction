f53db70ba814a3e46d7f2d1a9997b5932e1303ce
Guillem Hurault <39564817+ghurault@users.noreply.github.com>
2019-09-08 21:17:29 +0100

    Update functions.R

--- functions.R
+++ functions.R
@@ -24,3 +24,38 @@ heatmap_results <- function(df) {
     theme_classic(base_size = 15) +
     theme(axis.text.x = element_text(angle = 90))
 }
+
+# Extract parameters from Stan model --------------------------------------
+
+extract_parameters <- function(fit, param, param_ind, param_obs, teams, data_stan) {
+  # Extract parameters' summary
+  #
+  # Args:
+  # fit: stanfit object
+  # param: parameters to extract
+  # param_ind: individual parameters in param
+  # param_obs
+  # teams: vector of team ID
+  # data_stan: data input to the stan function
+  #
+  # Returns: dataframe containing posterior summary statistics of the parameters 
+  
+  par <- HuraultMisc::summary_statistics(fit, param)
+  par$Team <- NA
+  par$Game <- NA
+
+  ## Team dependent parameters
+  for (i in intersect(param_ind, param)) {
+    idx <- which(par$Variable == i)
+    par$Team[idx] <- teams[par$Index[idx]]
+  }
+  
+  ## Game dependent parameters
+  for (i in intersect(param_obs, param)) {
+    idx <- which(par$Variable == i)
+    par$Game[idx] <- par$Index[idx]
+  }
+
+  par$Index <- NULL
+  return(par)
+}
