3203074722f6182d16672dc0dd530f85744d456c
Guillem Hurault <39564817+ghurault@users.noreply.github.com>
2019-09-09 22:01:34 +0100

    Add football_stats

--- functions.R
+++ functions.R
@@ -59,3 +59,73 @@ extract_parameters <- function(fit, param, param_ind, param_obs, teams, data_sta
   par$Index <- NULL
   return(par)
 }
+
+# Football statistics -----------------------------------------------------
+
+football_stats <- function(df) {
+  # Extract stats from dataframe of games
+  #
+  # Args:
+  # df: Dataframe of games
+  #
+  # Returns:
+  # Datafrane of football statistics
+  
+  teams <- with(df, sort(unique(c(as.character(HomeTeam), as.character(AwayTeam)))))
+  
+  # Win
+  N_win_home <- sapply(teams,
+                       function(teamName) {
+                         with(subset(df, HomeTeam == teamName), sum(FTR ==  "H"))
+                       })
+  N_win_away <- sapply(teams,
+                       function(teamName) {
+                         with(subset(df, AwayTeam == teamName), sum(FTR ==  "A"))
+                       })
+  # Draw
+  N_draw_home <- sapply(teams,
+                        function(teamName) {
+                          with(subset(df, HomeTeam == teamName), sum(FTR ==  "D"))
+                        })
+  N_draw_away <- sapply(teams,
+                        function(teamName) {
+                          with(subset(df, AwayTeam == teamName), sum(FTR ==  "D"))
+                        })
+  # Lose
+  N_lose_home <- sapply(teams,
+                        function(teamName) {
+                          with(subset(df, HomeTeam == teamName), sum(FTR ==  "A"))
+                        })
+  N_lose_away <- sapply(teams,
+                        function(teamName) {
+                          with(subset(df, AwayTeam == teamName), sum(FTR ==  "H"))
+                        })
+  # Goal
+  N_goal_home <- sapply(teams,
+                        function(teamName) {
+                          with(subset(df, HomeTeam == teamName), sum(FTHG))
+                        })
+  N_goal_away <- sapply(teams,
+                        function(teamName) {
+                          with(subset(df, AwayTeam == teamName), sum(FTAG))
+                        })
+  
+  # Aggregates
+  N_win <- N_win_home + N_win_away
+  N_draw <- N_draw_home + N_draw_away
+  N_lose <- N_lose_home + N_lose_away
+  N_goal <- N_goal_home + N_goal_away
+  N_point <- 3 * N_win + N_draw
+  
+  out <- data.frame(Team = teams,
+                    N_win_home, N_win_away, N_win,
+                    N_draw_home, N_draw_away, N_draw,
+                    N_lose_home, N_lose_away, N_lose,
+                    N_goal_home, N_goal_away, N_goal,
+                    N_point)
+  out <- merge(out,
+               data.frame(Team = teams[order(N_point, N_goal, decreasing = TRUE)],
+                          rank = 1:length(teams)), # rank
+               by = "Team")
+  return(out)
+}
