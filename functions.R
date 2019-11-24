ae008593e65c470e203854bcc25d1298369b0e27
Guillem Hurault <39564817+ghurault@users.noreply.github.com>
2019-11-24 16:24:33 +0000

    Finalise prior_pred_check

--- functions.R
+++ functions.R
@@ -328,3 +328,341 @@ stackhist_rank <- function(rank_rep, teams) {
     labs(y = "Cumulative probability") +
     theme_classic(base_size = 20)
 }
+
+# Validation --------------------------------------------------------------
+
+process_predictions <- function(fit, id) {
+  # Compute predicted probability for goals scored and game results
+  #
+  # Args:
+  # fit: stanfit object
+  # id: Dataframe of game id
+  #
+  # Returns:
+  # Dataframe in tall format
+  
+  home_goals <- rstan::extract(fit, pars = "home_goals_test")[[1]]
+  away_goals <- rstan::extract(fit, pars = "away_goals_test")[[1]]
+  
+  # FTR
+  pred1 <- id
+  pred1$H <- apply(home_goals - away_goals, 2, function(x) {mean(x > 0)})
+  pred1$A <- apply(home_goals - away_goals, 2, function(x) {mean(x < 0)})
+  pred1$D <- apply(home_goals - away_goals, 2, function(x) {mean(x == 0)})
+  pred1 <- reshape2::melt(pred1, id.vars = c("Game", "HomeTeam", "AwayTeam"), variable.name = "Value", value.name = "Probability")
+  pred1$Variable <- "FTR"
+  
+  # Goals
+  pred_goals <- function(goals, lbl) {
+    # Compute dataframe for predicted number of goals
+    n_post <- nrow(goals) # Number of samples
+    out <- do.call(rbind,
+                   lapply(1:nrow(id),
+                          function(i) {
+                            tmp <- table(goals[, i]) / n_post
+                            tmp <- as.data.frame(tmp)
+                            colnames(tmp) <- c("Value", "Probability")
+                            cbind(id[i, ], tmp)
+                          }))
+    out$Variable <- lbl
+    return(out)
+  }
+  pred2 <- pred_goals(home_goals, "FTHG")
+  pred3 <- pred_goals(away_goals, "FTAG")
+  
+  rbind(pred1, pred2, pred3)
+}
+
+prepare_predictions <- function(pred, act, test_game, var = "FTR") {
+  # Select games in testing set,
+  # and compute forecast and actual dataframe/matrices for variable var.
+  # This function is an intermediate step for computing metrics.
+  #
+  # Args:
+  # pred: prediction dataframe
+  # act: actual (observed outcome) dataframe
+  # test_game: vector of test game ID
+  # var: character corresponding to the variable to consider: FTR, FTHG or FTAG
+  #
+  # Returns:
+  # List containing Forecast (prediction) and Actual dataframe
+  
+  if (!(var %in% c("FTR", "FTHG", "FTAG"))) {
+    stop("var should be either `FTR`, `FTHG` or `FTAG`.")
+  }
+  
+  # Select game
+  pred <- pred[(pred$Game %in% test_game) & (pred$Variable == var), ]
+  act <- act[act$Game %in% test_game, ]
+  
+  if (var == "FTR") {
+    # Order FTR outcomes
+    pred$Value <- factor(pred$Value, levels = c("A", "D", "H"))
+  } else {
+    # Convert number of goals to numeric
+    pred$Value <- as.numeric(as.character(pred$Value))
+    max_pred_goal <- max(pred$Value)
+    # Pad with 0 if some goal values are missing between 1:max_pred_goal so correct 0 padding later 
+    pred <- rbind(pred,
+                  do.call(rbind,
+                          lapply(setdiff(1:max_pred_goal, unique(pred$Value)),
+                                 function(i) {
+                                   tmp <- pred[1, ]
+                                   tmp[, c("Value", "Probability")] <- c(i, 0)
+                                   return(tmp)
+                                 })))
+  }
+  
+  # Reshape dataframe
+  pred <- reshape2::dcast(pred, Game + HomeTeam + AwayTeam ~ Value, value.var = "Probability")
+  if (var != "FTR") {
+    # Pad with 0 probabilities
+    pred[is.na(pred)] <- 0
+    # Add extra 0 columns if max predicted goals below max observed goals
+    max_obs_goal <- max(act[, var])
+    if (max_pred_goal < max_obs_goal) {
+      for (i in (max_pred_goal + 1):max_obs_goal) {
+        pred[, as.character(i)] <- rep(0, nrow(pred))
+      }
+    }
+  }
+  
+  # Generate similar matrix for actual outcomes
+  act <- merge(act[, c("Game", "HomeTeam", "AwayTeam", var)],
+               pred,
+               by = c("Game", "HomeTeam", "AwayTeam"))
+  outcome_id <-  !(colnames(act) %in%  c("Game", "HomeTeam", "AwayTeam", var))
+  act[, outcome_id] <- 0
+  for (i in 1:nrow(act)) {
+    act[i, as.character(act[i, var])] <- 1
+  }
+  act[[var]] <- NULL
+  
+  # Reorder
+  pred <- pred[with(pred, order(Game, HomeTeam, AwayTeam)), ]
+  act <- act[with(act, order(Game, HomeTeam, AwayTeam)), ]
+  
+  return(list(Forecast = pred, Actual = act))
+}
+
+compute_metrics <- function(pred, act, test_game, var) {
+  # Compute metrics
+  #
+  # Args:
+  # pred: prediction dataframe
+  # act: actual (observed outcome) dataframe
+  # test_game: vector of test game ID
+  # var: character corresponding to the variable to consider: FTR, FTHG or FTAG
+  #
+  # Returns:
+  # Dataframe of metrics
+  
+  if (var == "FTG") {
+    # Combine FTHG and FTAG
+    l1 <- prepare_predictions(pred, act, test_game, "FTHG")
+    l2 <- prepare_predictions(pred, act, test_game, "FTAG")
+    f1 <- l1$Forecast
+    a1 <- l1$Actual
+    f2 <- l2$Forecast
+    a2 <- l2$Actual
+    
+    mg1 <- ncol(f1) - 3 - 1 # max goal for 1
+    mg2 <- ncol(f2) - 3 - 1 # max goal for 2
+    if (mg1 > mg2) {
+      # Pad f2 and a2 with 0
+      f2[, as.character(mg2 + 1):mg1] <- 0
+      a2[, as.character(mg2 + 1):mg1] <- 0
+    } else if (mg2 > mg1) {
+      # Pad f1 and a1 with 0
+      f1[, as.character((mg1 + 1):mg2)] <- 0
+      a1[, as.character((mg1 + 1):mg2)] <- 0
+    }
+    f <- rbind(f1, f2)
+    a <- rbind(a1, a2)
+  } else {
+    l <- prepare_predictions(pred, act, test_game, var)
+    f <- l$Forecast
+    a <- l$Actual
+  }
+  
+  col_id <- (colnames(f) %in% c("Game", "HomeTeam", "AwayTeam"))
+  Forecast <- as.matrix(f[, !col_id])
+  Actual <- as.matrix(a[, !col_id])
+  CumForecast <- t(apply(Forecast, 1, cumsum))
+  CumActual <- t(apply(Actual, 1, cumsum))
+  
+  K <- ncol(Forecast) # somewhat arbitrary for goals
+  d <- min(Forecast[Forecast != 0]) / 100 # to avoid log(0)
+  
+  RPS <- apply((CumActual - CumForecast)^2, 1, sum) / (K - 1)
+  CumLogLoss <- -apply(CumActual * log(pmax(CumForecast, d)), 1, sum) / (K - 1)
+  
+  BrierScore <- apply((Actual - Forecast)^2, 1, mean) # Between 0 and 2 (regardless of the number of categories)
+  LogLoss <- -apply(Actual * log(Forecast + d), 1, mean)
+  
+  # Return metric per prediction (need to include additional info for FTG then) or average?
+  data.frame(Metric = c("RPS", "CumLogLoss", "BrierScore", "LogLoss"),
+             Value = sapply(list(RPS, CumLogLoss, BrierScore, LogLoss), mean),
+             Variable = var)
+  # cbind(f[, col_id],
+  #       data.frame(RPS, CumLogLoss, BrierScore, LogLoss))
+}
+
+plot_lift <- function(prep_pred, best_bet = FALSE) {
+  # Plot lift curve
+  #
+  # Args:
+  # prep_pred: List containing Forecast and Actual dataframe (output from prepare_predictions)
+  # best_bet: whether to compute the lift for the best bet (regardless of whether it's A, D or H; 0, 1, 2, ...)
+  #
+  # Returns:
+  # Ggplot
+  
+  library(ggplot2)
+  id_lbl <- c("Game", "HomeTeam", "AwayTeam")
+  val <- setdiff(colnames(prep_pred$Forecast), id_lbl)
+  
+  compute_lift <- function(f, a, p0) {
+    # Compute lift
+    #
+    # Args:
+    # f: dataframe of forecast
+    # a: dataframe of actual
+    # p0: base rate
+    #
+    # Returns:
+    # Dataframe of lift
+    
+    tmp <- merge(f, a, by = id_lbl)
+    tmp <- tmp[order(tmp$Forecast, decreasing = TRUE), ]
+    
+    PropBet <- (1:nrow(tmp)) / nrow(tmp)
+    PropWin <- cumsum(tmp$Actual) / (1:nrow(tmp))
+    Lift <- PropWin / p0
+    
+    data.frame(Game = tmp$Game, PropBet, PropWin, Lift)
+  }
+  
+  if (!best_bet) {
+    
+    if (length(val) < 9) {
+      palette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    } else {
+      getPalette <- colorRampPalette(rev(RColorBrewer::brewer.pal(9, "Set1"))) # to extend colour palette
+      palette <- getPalette(length(val))
+    }
+    
+    lift <- do.call(rbind,
+                    lapply(val,
+                           function(x) {
+                             f <- prep_pred$Forecast[, c(id_lbl, x)]
+                             f <- change_colnames(f, x, "Forecast")
+                             a <- prep_pred$Actual[, c(id_lbl, x)]
+                             a <- change_colnames(a, x, "Actual")
+                             
+                             tmp <- compute_lift(f, a, p0 = mean(a$Actual))
+                             tmp$Value <- x
+                             return(tmp)
+                           }))
+    lift$Value <- factor(lift$Value, levels = val)
+    
+    p <- ggplot(data = lift, aes(x = PropBet, y = Lift, colour = Value)) +
+      scale_colour_manual(values = palette)
+    
+  } else {
+    id <- apply(prep_pred$Forecast[, val], 1, which.max) # Bet on highest probability outcome
+    f <- prep_pred$Forecast[, id_lbl]
+    Value <- val[id]
+    f$Forecast <- NA
+    a <- prep_pred$Actual[, id_lbl]
+    a$Actual <- NA
+    
+    for (i in 1:nrow(f)) {
+      f$Forecast[i] <- prep_pred$Forecast[i, Value[i]]
+      a$Actual[i] <- prep_pred$Actual[i, Value[i]]
+    }
+    
+    lift <- compute_lift(f, a, p0 = 1 / length(val)) # p0 might be subject to discussion here
+    lift$Value <- Value
+    
+    p <- ggplot(data = lift, aes(x = PropBet, y = Lift))
+  }
+  
+  p +
+    geom_line() +
+    geom_hline(yintercept = 1) +
+    labs(colour = "") +
+    theme_bw(base_size = 15)
+  
+}
+
+plot_calibration <- function(prep_pred, CI = NULL, pool = FALSE) {
+  # Plot calibration
+  #
+  # Args:
+  # prep_pred: List containing Forecast and Actual dataframe (output from prepare_predictions)
+  # CI: confidence level in %. If NULL, confidence intervals are not computed.
+  # pool: whether to pool/combine the values for the plot
+  #
+  # Returns:
+  # Ggplot
+  
+  library(ggplot2)
+  id_lbl <- c("Game", "HomeTeam", "AwayTeam")
+  val <- setdiff(colnames(prep_pred$Forecast), id_lbl)
+  
+  if (!pool) {
+    cal <- do.call(rbind,
+                   lapply(val,
+                          function(x) {
+                            tmp <- HuraultMisc::compute_calibration(prep_pred$Forecast[, x],
+                                                                    prep_pred$Actual[, x],
+                                                                    method = "smoothing",
+                                                                    CI = CI)
+                            tmp$Value <- x
+                            return(tmp)
+                          }))
+    
+    if (length(val) < 8) {
+      palette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    } else {
+      getPalette <- colorRampPalette(rev(RColorBrewer::brewer.pal(9, "Set1"))) # to extend colour palette
+      palette <- getPalette(length(val))
+    }
+    
+    p <- ggplot(data = cal, aes(x = Forecast, y = Frequency, colour = Value)) +
+      scale_colour_manual(values = palette)
+    
+  } else {
+    
+    f <- prep_pred$Forecast[, val]
+    a <- prep_pred$Actual[, val]
+    cal <- HuraultMisc::compute_calibration(c(as.matrix(f)),
+                                            c(as.matrix(a)),
+                                            method = "smoothing",
+                                            CI = CI)
+    
+    p <- ggplot(data = cal, aes(x = Forecast, y = Frequency))
+    
+  }
+  
+  p <- p +
+    geom_line() +
+    geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+    coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
+    labs(colour = "", fill = "") +
+    theme_bw(base_size = 15)
+  
+  if (!is.null(CI)) {
+    if (!pool) {
+      p <- p +
+        geom_ribbon(aes(ymin = Lower, ymax = Upper, fill = Value), alpha = 0.5) +
+        scale_fill_manual(values = palette)
+    } else {
+      p <- p +
+        geom_ribbon(aes(ymin = Lower, ymax = Upper), alpha = 0.5)
+    }
+  }
+  return(p)
+  
+}
